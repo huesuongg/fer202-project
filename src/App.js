a658d5151398253b6434e0b022e44170d713b56b
Nguyen Hue Suong <suongnhde180706@fpt.edu.vn>
2025-07-17 22:17:09 +0700

    feat: finish all feature

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { useEffect } from "react";
 import AppRouter from "./router/landing-router"
 import { useDispatch, useSelector } from "react-redux";
 import { checkAuthStatus } from "./features/authenticate/authThunk";
+import axiosInstance from "./services/axiosInstance";
+import { setCategories } from "./services/categorySlice";
 
 function App() {
     const dispatch = useDispatch();
-    // const authStatus = useSelector((state) => state.auth.isAuthenticated)
-    const authStatus = true;
-    useEffect(()=> {
-        if(authStatus === null){
-        dispatch(checkAuthStatus());
-    }
-    }, [])
     
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const res = await axiosInstance.get('/ServiceCategory')
+                dispatch(setCategories(res.data));
+            } catch (error) {
+                console.error('Lỗi khi fetch categories:', error);
+            }
+        };
+
+        fetchCategories();
+    }, [dispatch]);
+
     return (
-        
-            <div className="App">
-             <AppRouter/>
-            </div>
+
+        <div className="App">
+            <AppRouter />
+        </div>
     );
 }
 
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -7,14 +7,13 @@ export const LandingNavbar = () => {
   return (
     <header className="main-header">
       <div className="logo">
-        <span className="logo-hat">Unknown</span><span className="logo-de">Project</span>
+        <span className="logo-hat">Hat</span><span className="logo-de">De</span>
       </div>
 
       <nav className="nav-menu">
-        <a href="#">Home</a>
-        <a href="#">Nav 1</a>
-        <a href="#">Nav 2</a>
-        <a href="#">Nav 3</a>
+        <a href="../">Trang chủ</a>
+        <a href="../wedding">Dịch vụ</a>
+        <a href="../accessory">Phụ kiện trang trí</a>
       </nav>
 
       <div className="search-bar">
@@ -24,8 +23,8 @@ export const LandingNavbar = () => {
 
       <div className="nav-button">
         {/* <i className="fas fa-user"></i> */}
-        <a href="./login">Log in</a>
-        <a href="./register">Sign up</a>
+        <a href="../login">Đăng nhập</a>
+        <a href="../register">Đăng ký</a>
       </div>
     </header>
   );
@@ -33,6 +32,8 @@ export const LandingNavbar = () => {
   
 
   export const HomeNavbar = () => {
+    
+    
     return (
       <header className="main-header">
         <div className="logo">
@@ -40,21 +41,83 @@ export const LandingNavbar = () => {
         </div>
   
         <nav className="nav-menu">
-        <a href="#">Home</a>
-        <a href="#">Nav 1</a>
-        <a href="#">Nav 2</a>
-        <a href="#">Nav 3</a>
+
+        <a href="../">Trang chủ</a>
+        <a href="../wedding">Dịch vụ</a>
+        <a href="../accessory">Phụ kiện trang trí</a>
       </nav>
+
   
         <div className="search-bar">
           <i className="fas fa-search"></i>
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Tìm kiếm..." />
         </div>
   
         <div className="nav-icons">
-          <i className="far fa-heart"></i>
-          <i className="fas fa-shopping-cart"></i>
-          <i className="fas fa-user"></i>
+          <a href="#"><i className="far fa-heart"></i></a>
+          <a href="../cart"><i className="fas fa-shopping-cart"></i></a>
+          <a href="../profile"><i className="fas fa-user"></i></a>
+        </div>
+      </header>
+    );
+  };
+
+
+
+  
+  export const VendorDashboardNavbar = () => {
+    return (
+      <header className="main-header">
+        <div className="logo">
+          <span className="logo-hat">Hat</span><span className="logo-de">De</span>
+        </div>
+  
+        <nav className="nav-menu">
+        <a href="../dashboard">Doanh thu</a>
+        <a href="../shop">Cửa hàng của tôi</a>
+        <a href="../booking">Đơn hàng</a>
+        <a href="../discount">Khuyến mại</a>
+
+      </nav>
+
+  
+        <div className="search-bar">
+          <i className="fas fa-search"></i>
+          <input type="text" placeholder="Tìm kiếm..." />
+        </div>
+  
+        <div className="nav-icons">
+          <a href="../profile"><i className="fas fa-user"></i></a>
+        </div>
+      </header>
+    );
+  };
+
+
+  
+  export const AdminDashboardNavbar = () => {
+    return (
+      <header className="main-header">
+        <div className="logo">
+          <span className="logo-hat">Hat</span><span className="logo-de">De</span>
+        </div>
+  
+        <nav className="nav-menu">
+        <a href="../">Trang chủ</a>
+        <a href="../user">Cửa hàng của tôi</a>
+        <a href="../booking">Đơn hàng</a>
+        <a href="../discount">Khuyến mại</a>
+
+      </nav>
+
+  
+        <div className="search-bar">
+          <i className="fas fa-search"></i>
+          <input type="text" placeholder="Tìm kiếm..." />
+        </div>
+  
+        <div className="nav-icons">
+          <a href="../profile"><i className="fas fa-user"></i></a>
         </div>
       </header>
     );
